248ede320f7a81979dbd1863e79b381287e7c544
devgeon <devgeon.git@gmail.com>
2022-05-05 12:13:32 +0000

    Solve BOJ-25083-새싹.c

---
+++ Basic-Syntax/BOJ-25083-새싹.c
@@ -0,0 +1,18 @@
+// BOJ-25083 / 새싹
+// devgeon, 2022.05.05, C99
+// https://www.acmicpc.net/problem/25083
+
+// 예제와 같이 새싹을 출력하시오.
+
+#include<stdio.h>
+
+int main()
+{
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+	return 0;
+}
