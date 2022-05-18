f9c8c5448b6790beecdf25ea3ae39002e28192c3
devgeon <devgeon.git@gmail.com>
2022-05-18 23:46:51 +0000

    Submit BOJ-01072-게임.c

--- Brute-Force/BOJ-01072-게임.c
+++ Brute-Force/BOJ-01072-게임.c
@@ -1,5 +1,5 @@
 // BOJ-01072 / 게임
-// devgeon, 2022.05.11, C99
+// devgeon, 2022.05.19, C99
 // https://www.acmicpc.net/problem/1072
  
 // 앞으로의 게임에서 무조건 승리한다고 가정할 때, 게임을 최소 몇 판을 더 해야 승률이 변하는지 구하는 프로그램을 작성하시오.
@@ -11,6 +11,14 @@
 
 // 입력: 첫째 줄에 정수 X와 Y가 주어진다.
 // 출력: 첫째 줄에 게임을 최소 몇 판 더 해야하는지 출력한다.
+
+
+// (z*100)+1 = (y+c/x+c)*100   (z,x,y는 상수)
+// (x+c)(100z+1) = (y+c)*100
+// 100xz + x + 100cz +c = 100y + 100c
+// 100cz - 99c = 100y - 100xz - x
+// c(100z-99) = 100y-100xz-x
+// c = (100y-100xz-x) / (100z-99)   (z ≠ 0.99)
 
 
 #include<stdio.h>
@@ -29,14 +37,14 @@ int main()
 	if(win_rate>=99) {
 		printf("-1\n");
 		return 0;
+	} else {
+		i = (100*win_count - play_count*win_rate - play_count) / (win_rate-99);
+		if(win_count>0) {
+			i++;
+		}
 	}
 	
-	do {
-		temp = (win_count+i) * 100 / (play_count+i);
-		i++;
-	} while(win_rate==temp);
-		
-	printf("%lld\n", i-1);
+	printf("%lld\n", i);
 	
 	return 0;
 }
