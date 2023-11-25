203f172c87b75fb282d9b12c0033bd176a19e97a
devgeon <devgeon.git@gmail.com>
2023-11-25 17:04:26 +0000

    Refactor BOJ-07569-토마토.cpp

--- Graphs/Breadth-First-Search/BOJ-07569-토마토.cpp
+++ Graphs/Breadth-First-Search/BOJ-07569-토마토.cpp
@@ -26,6 +26,7 @@ int main() {
     vector<vector<vector<int>>> box(
         length, vector<vector<int>>(width, vector<int>(height, -1)));
     queue<Position> queue;
+    vector around_idx = {1, -1, 0, 0, 0, 0};
 
     for (int z = 0; z < height; z++) {
         for (int y = 0; y < length; y++) {
@@ -48,41 +49,22 @@ int main() {
             queue.pop();
             continue;
         }
-        x = queue.front().x;
-        y = queue.front().y;
-        z = queue.front().z;
-        queue.pop();
 
-        if (0 <= x - 1 && box[y][x - 1][z] == 0) {
-            box[y][x - 1][z] = 1;
-            unriped_tomato--;
-            queue.push(Position(x - 1, y, z));
+        for (int i = 0; i < 6; i++) {
+            x = queue.front().x + around_idx[i];
+            y = queue.front().y + around_idx[(i + 2) % 6];
+            z = queue.front().z + around_idx[(i + 4) % 6];
+
+            if ((i & 1 && 0 <= x && 0 <= y && 0 <= z) ||
+                (!(i & 1) && x < width && y < length && z < height)) {
+                if (box[y][x][z] == 0) {
+                    box[y][x][z] = 1;
+                    unriped_tomato--;
+                    queue.push(Position(x, y, z));
+                }
+            }
         }
-        if (x + 1 < width && box[y][x + 1][z] == 0) {
-            box[y][x + 1][z] = 1;
-            unriped_tomato--;
-            queue.push(Position(x + 1, y, z));
-        }
-        if (0 <= y - 1 && box[y - 1][x][z] == 0) {
-            box[y - 1][x][z] = 1;
-            unriped_tomato--;
-            queue.push(Position(x, y - 1, z));
-        }
-        if (y + 1 < length && box[y + 1][x][z] == 0) {
-            box[y + 1][x][z] = 1;
-            unriped_tomato--;
-            queue.push(Position(x, y + 1, z));
-        }
-        if (0 <= z - 1 && box[y][x][z - 1] == 0) {
-            box[y][x][z - 1] = 1;
-            unriped_tomato--;
-            queue.push(Position(x, y, z - 1));
-        }
-        if (z + 1 < height && box[y][x][z + 1] == 0) {
-            box[y][x][z + 1] = 1;
-            unriped_tomato--;
-            queue.push(Position(x, y, z + 1));
-        }
+        queue.pop();
     }
 
     if (unriped_tomato > 0) {
