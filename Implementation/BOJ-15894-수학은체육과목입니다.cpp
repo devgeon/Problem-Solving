c0e8a762874de9d4b0ab6ffa24bf651133021cee
devgeon <devgeon.git@gmail.com>
2024-01-09 14:27:37 +0000

    Solve BOJ-15894-수학은체육과목입니다.cpp

---
+++ Implementation/BOJ-15894-수학은체육과목입니다.cpp
@@ -0,0 +1,14 @@
+// BOJ-15894 / 수학은 체육과목 입니다
+// devgeon, 2024.01.09, C++17
+// https://www.acmicpc.net/problem/15894
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n = 0;
+    cin >> n;
+    cout << n * 4LL << endl;
+    return 0;
+}
