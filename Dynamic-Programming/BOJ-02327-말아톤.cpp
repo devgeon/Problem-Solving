13cf016f45f1b85021ed7c0f0b0167f7b270d9d8
devgeon <devgeon.git@gmail.com>
2024-01-21 14:32:44 +0000

    Refactor BOJ-02327-말아톤.cpp

--- Dynamic-Programming/BOJ-02327-말아톤.cpp
+++ Dynamic-Programming/BOJ-02327-말아톤.cpp
@@ -14,8 +14,8 @@ int main() {
     const int HEIGHT = 0, SPEED = 1;
     int height_requirement = 0, num_of_students = 0, height = 0, speed = 0;
     cin >> height_requirement >> num_of_students;
+    vector<int> table(height_requirement, -1);
     vector<vector<int>> students(num_of_students, vector<int>(2, 0));
-    vector<vector<int>> table(2, vector<int>(height_requirement, -1));
     for (int i = 0; i < num_of_students; i++) {
         cin >> students[i][HEIGHT] >> students[i][SPEED];
     }
@@ -23,19 +23,17 @@ int main() {
     for (int r = 0; r < num_of_students; r++) {
         height = students[r][HEIGHT];
         speed = students[r][SPEED];
-        for (int c = 0; c < height_requirement; c++) {
-            table[r % 2][c] = table[(r + 1) % 2][c];
+        for (int c = height_requirement - 1; c >= 0; c--) {
             if (c >= height) {
-                table[r % 2][c] =
-                    max(table[r % 2][c],
-                        min(table[(r + 1) % 2][c - height], speed));
+                table[c] = max(table[c], min(table[c - height], speed));
             }
             if (c + 1 == height) {
-                table[r % 2][c] = max(table[r % 2][c], speed);
+                table[c] = max(table[c], speed);
+                break;
             }
         }
     }
 
-    cout << table[(num_of_students - 1) % 2][height_requirement - 1] << endl;
+    cout << table[height_requirement - 1] << endl;
     return 0;
 }
