4a07ca91b77196b9e7997f589d4b7f870e94c6e0
devgeon <devgeon.git@gmail.com>
2024-04-07 12:03:22 +0000

    Refactor BOJ-02805-나무자르기.cpp

--- Search/Binary-Search/BOJ-02805-나무자르기.cpp
+++ Search/Binary-Search/BOJ-02805-나무자르기.cpp
@@ -1,67 +1,48 @@
 // BOJ-02805 / 나무 자르기
-// devgeon, 2024.03.10, C++17
+// devgeon, 2024.04.07, C++17
 // https://www.acmicpc.net/problem/2805
 
-#include <algorithm>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-long long cut_down_trees(vector<int> &trees, int height);
+long long cut_down_trees(vector<int> &height_of_trees, int height);
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int num_of_trees = 0, required = 0;
-    int low = 0, high = 0, mid = 0, max_value = 0, answer = 0;
-    vector<int> trees;
-
+    int low = 0, high = 0, mid = 0;
+    int num_of_trees = 0, required = 0, max_value = 0;
     cin >> num_of_trees >> required;
-    trees.reserve(num_of_trees);
-    for (int i = 0, input = 0; i < num_of_trees; i++) {
-        cin >> input;
-        trees.push_back(input);
-        if (input > max_value) {
-            max_value = input;
-        }
+
+    vector<int> height_of_trees(num_of_trees, 0);
+    for (int &height : height_of_trees) {
+        cin >> height;
+        max_value = max(height, max_value);
     }
-    sort(trees.begin(), trees.end());
 
     high = max_value;
-    while (low <= high) {
-        mid = (high + low) / 2;
-        if (cut_down_trees(trees, mid) >= required) {
-            answer = max(answer, mid);
-            low = mid + 1;
-        } else {
+    while (low < high) {
+        mid = (high + low + 1) / 2;
+        if (cut_down_trees(height_of_trees, mid) < required) {
             high = mid - 1;
+        } else {
+            low = mid;
         }
     }
 
-    cout << answer << endl;
+    cout << low << endl;
 
     return 0;
 }
 
-long long cut_down_trees(vector<int> &trees, int height) {
-    int low = 0, high = 0, mid = 0, index = 0;
+long long cut_down_trees(vector<int> &height_of_trees, int height) {
     long long result = 0;
 
-    high = trees.size() - 1;
-    while (low <= high) {
-        mid = (high + low) / 2;
-        if (trees[mid] > height) {
-            index = mid;
-            high = mid - 1;
-        } else {
-            low = mid + 1;
-        }
-    }
-
-    for (int i = index; i < trees.size(); i++) {
-        result += trees[i] - height;
+    for (int i = 0; i < height_of_trees.size(); i++) {
+        result += max(height_of_trees[i] - height, 0);
     }
 
     return result;
