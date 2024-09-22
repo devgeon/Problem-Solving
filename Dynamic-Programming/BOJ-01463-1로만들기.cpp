a1a373839e4d10cd673cd4decb459ef08a419721
devgeon <devgeon.git@gmail.com>
2024-09-22 15:47:41 +0000

    Refactor BOJ-01463-1로만들기.cpp

--- Dynamic-Programming/BOJ-01463-1로만들기.cpp
+++ Dynamic-Programming/BOJ-01463-1로만들기.cpp
@@ -1,5 +1,5 @@
 // BOJ-01463 / 1로 만들기
-// devgeon, 2024.05.03, C++17
+// devgeon, 2024.09.22, C++17
 // https://www.acmicpc.net/problem/1463
 
 #include <iostream>
@@ -13,19 +13,19 @@ int main() {
     int num = 0;
     cin >> num;
 
-    vector<int> cache(num, MAX);
-    cache[0] = 0;
+    vector<int> cache(num + 1, MAX);
+    cache[1] = 0;
 
-    for (int i = 2; i <= num; i++) {
-        if (i % 3 == 0) {
-            cache[i - 1] = min(cache[i - 1], cache[i / 3 - 1] + 1);
+    for (int i = 1; i < num; i++) {
+        if (i * 2 <= num) {
+            cache[i * 2] = min(cache[i * 2], cache[i] + 1);
         }
-        if (i % 2 == 0) {
-            cache[i - 1] = min(cache[i - 1], cache[i / 2 - 1] + 1);
+        if (i * 3 <= num) {
+            cache[i * 3] = min(cache[i * 3], cache[i] + 1);
         }
-        cache[i - 1] = min(cache[i - 1], cache[i - 2] + 1);
+        cache[i + 1] = min(cache[i + 1], cache[i] + 1);
     }
 
-    cout << cache[num - 1] << endl;
+    cout << cache[num] << endl;
     return 0;
 }
