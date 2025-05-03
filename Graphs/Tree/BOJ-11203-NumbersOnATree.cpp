a1184e125d4d913097912c30b9ee032e65bfa304
devgeon <devgeon.git@gmail.com>
2025-05-03 13:11:32 +0000

    Solve BOJ-11203-NumbersOnATree.cpp

---
+++ Graphs/Tree/BOJ-11203-NumbersOnATree.cpp
@@ -0,0 +1,27 @@
+// BOJ-11203 / Numbers On a Tree
+// devgeon, 2024.05.03, C++17
+// https://www.acmicpc.net/problem/11203
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int height = 0, index = 1;
+    char direction = 0;
+
+    cin >> height;
+    while (cin >> direction) {
+        index <<= 1;
+        if (direction == 'R') {
+            index++;
+        }
+    }
+    index = (1 << height + 1) - index;
+
+    cout << index << endl;
+    return 0;
+}
